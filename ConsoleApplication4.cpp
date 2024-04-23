e8cb05eadd5d066f89524285d9c70abc9df974cd
PaladinASdark <167896211+PaladinASdark@users.noreply.github.com>
2024-04-23 19:15:48 +0300

    фича

---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+std::string isPositiv(std::string n) {
+    std::reverse(n.begin(), n.end());
+    return n;
+}
+
+int main()
+{
+    std::string n;
+    std::cin >> n;
+    std::cout << isPositiv(n);
+}
